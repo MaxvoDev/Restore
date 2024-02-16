dd43de999311e58e4e35342766a86ff1c1a8d394
Maxvo <maxvo25@gmail.com>
2024-02-17 10:13:12 +1030

    Fix ProductController

--- Back-End/API/Controllers/ProductController.cs
+++ Back-End/API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,23 +9,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductController(StoreContext context)
         {
-            this.context = context;
+            this._context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
