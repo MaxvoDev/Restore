2f5baa9b18cd738b744230a75324b189faca355f
Maxvo <maxvo25@gmail.com>
2024-02-17 09:52:43 +1030

    init sample database

--- Back-End/API/Program.cs
+++ Back-End/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,5 +28,19 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch(Exception ex)
+{
+    logger.LogError(ex, "A problem occurred during migration");
+}
 
 app.Run();
